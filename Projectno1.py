5d1d65bae7881c74440c8ee84070513cbfef6267
itsmohishahere <itsmohisha@gmail.com>
2026-08-15 19:33:34 +0100

    Update Projectno1.py

--- Projectno1.py
+++ Projectno1.py
@@ -1,13 +1,2 @@
-import pygame
-import sys
 
-pygame.init()
-
-class Window:
-    def __init__(self,width,height):
-#using inheritance
-        self.width=width
-        self.height= height
-        self.display.set_caption("Hello")
-    def background(self)
-        img= pghame.image.load()
+     
