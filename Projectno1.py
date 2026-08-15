c404c7fccaadb77189ae4d5fa7a0475ff86ac0ba
itsmohishahere <itsmohisha@gmail.com>
2026-08-15 19:21:00 +0100

    Add files via upload

---
+++ Projectno1.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+
+pygame.init()
+
+class Window:
+    def __init__(self,width,height):
+#using inheritance
+        self.width=width
+        self.height= height
+        self.display.set_caption("Hello")
+    def background(self)
+        img= pghame.image.load()
